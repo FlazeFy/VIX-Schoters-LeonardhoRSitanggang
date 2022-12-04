cdcd2908107ad484e2ff9aef02edaff72e4123b7
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-12-04 07:59:45 +0700

    Init kotlin

---
+++ app/src/main/java/com/example/vix_schooters_leonardho/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vix_schooters_leonardho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
